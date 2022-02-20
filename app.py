91a9f737243dac55324cdc0f7a9cd136318b1969
huskydj1 <husky.davin@gmail.com>
2022-02-20 12:10:18 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/query-example')
+def query_example():
+    return 'Query String Example'
+
+@app.route("/result", methods = ["GET"])
+def result():
+    print(requests.files)
+    data = request.files['audio_data'].read()
+    print(data)
+
+
+    
+    return 'https://avatars.dicebear.com/api/bottts/:seed.svg'
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
