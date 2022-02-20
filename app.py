697856ee274e90f1ec26e7bfa647972e85475668
Davin <42313131+huskydj1@users.noreply.github.com>
2022-02-20 09:11:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def result():
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=80)
